1908d8aff5c2750681f89d4e620b51283efa3d28
HUST-SE-LY <1596087252@qq.com>
2023-09-20 12:15:37 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <h1>基础要求参考</h1>
+  <video src="/基础功能.mov" class="video" controls></video>
+  <h1>查看详情功能参考</h1>
+  <video src="/查看详情.mov" class="video" controls></video>
+  <h1>删除日记功能参考</h1>
+  <video src="/delete.mov" class="video" controls></video>
+  <h1>添加心情功能参考</h1>
+  <video src="/心情按钮.mov" class="video" controls></video>
+  <h1>暗色模式功能参考</h1>
+  <img src="/day.png" class="video" >
+  <img src="/dusk.png" class="video" >
+  <img src="/night.png" class="video" >
+</div>
+</template>
+
+<style scoped>
+.video {
+  width: 300px;
+}
+</style>
