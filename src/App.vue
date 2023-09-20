cc9ea769eb2e09c51d5220b258bda11a57925486
HUST-SE-LY <1596087252@qq.com>
2023-09-20 12:18:24 +0800

    update video

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
 <template>
 <div>
   <h1>基础要求参考</h1>
-  <video src="/基础功能.mov" class="video" controls></video>
+  <video src="/basic.mov" class="video" controls></video>
   <h1>查看详情功能参考</h1>
-  <video src="/查看详情.mov" class="video" controls></video>
+  <video src="/detail.mov" class="video" controls></video>
   <h1>删除日记功能参考</h1>
   <video src="/delete.mov" class="video" controls></video>
   <h1>添加心情功能参考</h1>
-  <video src="/心情按钮.mov" class="video" controls></video>
+  <video src="/moood.mov" class="video" controls></video>
   <h1>暗色模式功能参考</h1>
   <img src="/day.png" class="video" >
   <img src="/dusk.png" class="video" >
